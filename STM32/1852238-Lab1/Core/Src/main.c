cf0ba01f63091c36b896fef196827a307949c304
notsotalented <104625133+notsotalented@users.noreply.github.com>
2022-11-04 20:00:34 +0700

    Lab1-Exercise9

--- STM32/1852238-Lab1/Core/Src/main.c
+++ STM32/1852238-Lab1/Core/Src/main.c
@@ -147,22 +147,73 @@ int main(void)
 		  HAL_GPIO_WritePin(SEG11_GPIO_Port, SEG11_Pin, SET);
 		  break;
 	  }
+  }
 
+  //Set segment off function
+  void clearNumberOnClock(int num) {
+	  switch (num) {
+	  case 0:
+		  HAL_GPIO_WritePin(SEG0_GPIO_Port, SEG0_Pin, RESET);
+		  break;
+	  case 1:
+		  HAL_GPIO_WritePin(SEG1_GPIO_Port, SEG1_Pin, RESET);
+		  break;
+	  case 2:
+		  HAL_GPIO_WritePin(SEG2_GPIO_Port, SEG2_Pin, RESET);
+		  break;
+	  case 3:
+		  HAL_GPIO_WritePin(SEG3_GPIO_Port, SEG3_Pin, RESET);
+		  break;
+	  case 4:
+		  HAL_GPIO_WritePin(SEG4_GPIO_Port, SEG4_Pin, RESET);
+		  break;
+	  case 5:
+		  HAL_GPIO_WritePin(SEG5_GPIO_Port, SEG5_Pin, RESET);
+		  break;
+	  case 6:
+		  HAL_GPIO_WritePin(SEG6_GPIO_Port, SEG6_Pin, RESET);
+		  break;
+	  case 7:
+		  HAL_GPIO_WritePin(SEG7_GPIO_Port, SEG7_Pin, RESET);
+		  break;
+	  case 8:
+		  HAL_GPIO_WritePin(SEG8_GPIO_Port, SEG8_Pin, RESET);
+		  break;
+	  case 9:
+		  HAL_GPIO_WritePin(SEG9_GPIO_Port, SEG9_Pin, RESET);
+		  break;
+	  case 10:
+		  HAL_GPIO_WritePin(SEG10_GPIO_Port, SEG10_Pin, RESET);
+		  break;
+	  case 11:
+		  HAL_GPIO_WritePin(SEG11_GPIO_Port, SEG11_Pin, RESET);
+		  break;
+	  }
   }
 
   //INIT timer
   int time = 0;
 
+  //Dummy call to clear warning
+  clearAllClock();
+
   while (1)
   {
-	  //Reset time and clock
+	  //Reset time
 	  if (time >=12) {
-		  clearAllClock();
 		  time = 0;
 	  }
 
 	  //Display number
 	  setNumberOnClock(time);
+
+	  //Clear the 2nd LED from current LED
+	  if (time >= 2) {
+		  clearNumberOnClock(time - 2);
+	  }
+	  else {
+		  clearNumberOnClock(10 + time);
+	  }
 
 	  //Time progression per 0.5s
 	  time++;
